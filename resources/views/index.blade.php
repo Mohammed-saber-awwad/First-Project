3690afa245ef0b804e5f7977b04ee19107cd037c
Mohammed AD <moad90300@gmail.com>
2022-08-01 13:01:44 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/37f0e86244.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/hover.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/media.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-light bg-light">
+            <img src="img/logo.png" alt="" class="navbar-brand">     
+            <ul class="nav justify-content-end">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="section-one">
+        <div class="home">
+            <h1>Hello There</h1>
+        <p>we are leon super creative &</p>
+        <p>minimal agency web template</p>
+        </div>
+    </div>
+    <div class="section-two"> 
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4">
+                    <i class="fas fa-magic fa-3x"></i>
+                    <h4>Your Product is Worldwide</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias consequuntur reprehenderit labore accusamus quaerat dolore molestias vero asperiores fuga! Eos unde culpa error sunt id ab perspiciatis tempore nam mollitia.</p>
+                </div>
+                <div class="col-lg-4 col-md-4">
+                    <i class="fas fa-magic fa-3x"></i>
+                    <h4>Tell Us Your Idea</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias consequuntur reprehenderit labore accusamus quaerat dolore molestias vero asperiores fuga! Eos unde culpa error sunt id ab perspiciatis tempore nam mollitia.</p>
+                </div>
+                <div class="col-lg-4 col-md-4">
+                    <i class="fas fa-magic fa-3x"></i>
+                    <h4>WE Will Do All The Work</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias consequuntur reprehenderit labore accusamus quaerat dolore molestias vero asperiores fuga! Eos unde culpa error sunt id ab perspiciatis tempore nam mollitia.</p>
+                </div>
+                
+            </div>
+
+        </div>
+    </div>
+    <div class="services">
+        <div class="container">
+            <h1>Services</h1>
+            <p class="pragragh">Don't buy , be productive</p>
+        <div class="row">
+            <div class="graphic col-lg-4 col-md-4">
+                <div class="srv">
+                <i class="fas fa-palette fa-2x"></i>
+                <h3>Graphic Design</h3>
+                </div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                <div class="design">
+                    <div class="srv">
+                        <i class="fab fa-sketch fa-2x"></i>
+                    <h3>Web Design</h3>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                </div>
+            </div>
+            
+            <div class="graphic col-lg-4 col-md-4">
+                <div class="srv">
+                    <i class="fas fa-vector-square fa-2x"></i>
+                <h3>UI & UX
+                </h3>
+                </div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, quam est aperiam obcaecati ad</p>
+                <div class="development">
+                    <div class="srv"> 
+                        <i class="fas fa-pencil-ruler fa-2x"></i>
+                    <h3>Web Development
+                    </h3>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, quam est aperiam obcaecati ad</p>
+                </div>
+            </div>
+            
+            
+            <div class="img col-lg-4 col-md-4">
+                <div class="cover">
+                </div>
+                <img src="img/services.jpg" alt="" class="animate__animated animate__fadeInRight">
+            </div>
+        </div>
+        </div>
+    </div>
+    <div class="">
+        <div class="container">
+            <h1>Portfolio</h1>
+            <p class="pragragh">If you do it right , it woll last forever.</p>
+            <div class="row">
+                <div class="card col-lg-4 col-md-4">
+                    <img src="img/portfolio-1.jpg" alt="">
+                    <h4>Your Product is Worldwide</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias consequuntur reprehenderit labore accusamus quaerat dolore molestias vero asperiores fuga! Eos unde culpa error sunt id ab perspiciatis tempore nam mollitia.</p>
+                </div>
+                <div class="card col-lg-4 col-md-4">
+                    <img src="img/portfolio-2.jpg" alt="">
+                    <h4>Tell Us Your Idea</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias consequuntur reprehenderit labore accusamus quaerat dolore molestias vero asperiores fuga! Eos unde culpa error sunt id ab perspiciatis tempore nam mollitia.</p>
+                </div>
+                <div class="card col-lg-4 col-md-4">
+                    <img src="img/portfolio-3.jpg" alt="">
+                    <h4>WE Will Do All The Work</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias consequuntur reprehenderit labore accusamus quaerat dolore molestias vero asperiores fuga! Eos unde culpa error sunt id ab perspiciatis tempore nam mollitia.</p>
+                </div>
+                
+            </div>
+            </div>
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="contact">
+        <div class="container">
+            <h1>Concat</h1>
+            <p class="pragragh">WE are born to create</p>
+
+            <div class="contact-home">
+                <div class="contact-h">
+                    <h2>Freel free to drop us aline at</h2>
+                    <h3>leonagency@mail.com</h3>
+                </div>
+                <div class="contact-c">
+                    <p>Find us social networks</p>
+                    <div class="contact-a">
+                    <a href="">youTube</a>
+                    <a href="">facebooke</a>
+                    <a href="">whatsapp</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <footer>
+        <p>@ 2021 <span>Leon</span> All on social networks</p>
+    </footer>
+</body>
+</html>
